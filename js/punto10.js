fc86bf31d968efa033a544c963053b207c379483
Padilla Alan <padillaalan555@gmail.com>
2021-12-20 10:27:40 -0300

    reto

---
+++ js/punto10.js
@@ -0,0 +1,3 @@
+const cantMillas = prompt('ingrese la cantidad de millas');
+
+alert(`En las ${cantMillas} hay ${cantMillas*1.634234}`);
---
+++ js/punto9.js
@@ -0,0 +1,9 @@
+const DiasTotal = parseInt(prompt('ingrese la cantidad de dias'));
+
+
+const horasTotal = DiasTotal*24;
+
+
+const minutos = DiasTotal*1440
+
+alert(`En ${cantDias} días, hay: ${sumaHoras} horas, ${sumaMinutos} minutos y ${sumaSegundos} segundos`);s
